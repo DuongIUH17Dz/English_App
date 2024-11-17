015a44fcb10a305719c5d10ecdf44c76ae99a488
DESKTOP-NQDA7TS\ThanhTu <tuthanh375@gmail.com>
2024-11-17 22:44:33 +0700

    restructure layout

--- app/(tabs)/dictionary.tsx
+++ app/(tabs)/dictionary.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-export default function practice () {
+export default function dictionary () {
   return (
     <View>
-       <Text>Practice </Text>
+       <Text>Dictionary </Text>
     </View>
   )
 }
